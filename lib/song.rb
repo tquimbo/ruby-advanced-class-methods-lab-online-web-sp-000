fd6838714f7148dba460747aab87c483b1e14b72
Tim Quimbo <tquimbo1@gmail.com>
2021-03-10 18:24:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,7 @@ class Song
 
   def self.new_from_filename(name)
     artist_name = name.split("-")
-    name = artist_name.collect do |row|
+    songs = artist_name.collect do |row|
     data = row.split(".")
     artist_name = data[0]
     name = data[1]
