90bb8be2f8bce010977185e4a9f39be59b68698c
Tim Quimbo <tquimbo1@gmail.com>
2021-03-10 16:18:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,7 +25,7 @@ class Song
     self.all.find{|song| song.name == name}
   end
 
-  def self.find_or_create_by_name
+  def self.find_or_create_by_name(name)
     if self.find_by_name
       return self.find_by_name
     else
