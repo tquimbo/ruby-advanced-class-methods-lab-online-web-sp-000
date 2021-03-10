518afcbdce177a18fff9663d962f96a10343db9e
Tim Quimbo <tquimbo1@gmail.com>
2021-03-10 15:37:47 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,57 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def self.create
+  song = self.new
+  song.save
+  return song
+  end
+
+  def self.new_by_name(name)
+    new = self.new
+    new.name = name
+    return new
+  end
+
+  def self.create_by_name(name)
+    new = self.new
+    new.name = name
+    new.save
+    return new
+  end
+
+  def self.find_by_name(name)
+    self.all.find{|person| person.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+  self.find_by_name
+  self.create_by_name
+
+  end
+
+  def self.alphabetical
+
+  end
+
+  def self.new_from_filename
+
+  end
+
+  def self.create_from_filename
+
+  end
+
   def self.all
-    @@all
+  @@all
+  end
+
+  def self.destory_all
+  self.all.clear
   end
 
   def save
-    self.class.all << self
+      self.class.all << self
   end
 
 end
