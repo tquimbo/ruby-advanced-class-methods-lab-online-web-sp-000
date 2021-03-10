a4b377f1f52aca428fff33b0b1edcd5345e843e6
Tim Quimbo <tquimbo1@gmail.com>
2021-03-10 16:01:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,12 +26,12 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-  self.find_by_name
-  self.create_by_name
-  if self.find_by_name == name
-    return name
-  else return self.create_by_name
-  end
+    self.find_by_name
+    self.create_by_name
+    if self.find_by_name == name
+      return name
+    else return self.create_by_name
+    end
   end
 
   def self.alphabetical
